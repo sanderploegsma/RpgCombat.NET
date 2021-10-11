534667374f898a43fb147446c505d53f947de5a0
Sander Ploegsma <sanderploegsma@gmail.com>
2021-10-11 12:15:06 +0200

    RPG Combat kata - iteration 2

---
+++ RpgCombat02.Test/DamageCharacterTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+
+namespace RpgCombat02.Test
+{
+    public class DamageCharacterTest
+    {
+        [Fact]
+        public void DamageIsSubtractedFromHealthOfVictim()
+        {
+            var attacker = new Character();
+            var victim = new Character();
+            
+            attacker.Damage(victim, 100);
+            
+            Assert.Equal(900, victim.Health);
+        }
+        
+        [Fact]
+        public void VictimRemainsAliveWhenDamageDoesNotExceedHealth()
+        {
+            var attacker = new Character();
+            var victim = new Character();
+            
+            attacker.Damage(victim, 100);
+            
+            Assert.Equal(CharacterStatus.Alive, victim.Status);
+        }
+
+        [Fact]
+        public void VictimDiesWhenDamageExceedsHealth()
+        {
+            var attacker = new Character();
+            var victim = new Character();
+            
+            attacker.Damage(victim, 9999);
+            
+            Assert.Equal(CharacterStatus.Dead, victim.Status);
+            Assert.Equal(0, victim.Health);
+        }
+
+        [Fact]
+        public void CharacterCannotDamageHimself()
+        {
+            var character = new Character();
+
+            Assert.Throws<InvalidOperationException>(() => character.Damage(character, 100));
+        }
+
+        [Fact]
+        public void DamageIsReducedWhenVictimIsMuchHigherLevel()
+        {
+            var attacker = new Character();
+            var victim = new Character()
+            {
+                Level = 6,
+            };
+            
+            attacker.Damage(victim, 100);
+            
+            Assert.Equal(950, victim.Health);
+        }
+
+        [Fact]
+        public void DamageIsIncreasedWhenAttackerIsMuchHigherLevel()
+        {
+            var attacker = new Character()
+            {
+                Level = 6,
+            };
+            var victim = new Character();
+            
+            attacker.Damage(victim, 100);
+            
+            Assert.Equal(850, victim.Health);
+        }
+    }
+}
---
+++ RpgCombat02.Test/HealCharacterTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xunit;
+
+namespace RpgCombat02.Test
+{
+    public class HealCharacterTest
+    {
+        [Fact]
+        public void DeadCharactersCannotBeHealed()
+        {
+            var character = new Character
+            {
+                Status = CharacterStatus.Dead
+            };
+
+            Assert.Throws<InvalidOperationException>(() => character.Heal(character, 100));
+        }
+
+        [Fact]
+        public void HealingIncreasesHealthOfCharacter()
+        {
+            var character = new Character
+            {
+                Health = 100
+            };
+
+            character.Heal(character, 100);
+            
+            Assert.Equal(200, character.Health);
+        }
+
+        [Fact]
+        public void CharacterHealthDoesNotExceed1000WhenHealed()
+        {
+            var character = new Character
+            {
+                Health = 999
+            };
+
+            character.Heal(character, 100);
+            
+            Assert.Equal(1000, character.Health);
+        }
+        
+        [Fact]
+        public void CharactersCannotHealOthers()
+        {
+            var source = new Character();
+            var target = new Character
+            {
+                Health = 999
+            };
+
+            Assert.Throws<InvalidOperationException>(() => source.Heal(target, 100));
+        }
+    }
+}
